ca185a32e12a3637ba563d15ddad233ed8576e09
박준홍 <junhq1234@cu.ac.kr>
2024-06-07 04:40:53 +0900

    Upload

---
+++ Server/src/index.php
@@ -0,0 +1,18 @@
+<!-- src/index.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SQL Injection Example</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <form action="login.php" method="post">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username"><br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password"><br>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
